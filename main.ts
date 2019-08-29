1e5521fc3d1b452499342f7b5a7f4d2008f25cc2
S-Morimo <36156147+S-Morimo@users.noreply.github.com>
2019-08-29 22:38:12 +0900

    Updates.

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
-   namespace comment {
-       //% blockId=show_strings block="コメント"
-       export function noaction(text: strings): void{
-
+//% weight=70 icon="\uf075" color=#555555 block="コメント"
+namespace comment {
+       //% blockId=show_strings block="コメント %v"
+       export function noaction(text: string): void{
        }
    }
